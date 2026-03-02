917ef9d195f19f59087c204129909b6fc3690a12
hosneysamir23-create <hosneysamir23@gmail.com>
2026-03-02 04:35:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.markdown('<style>body{direction:rtl;text-align:right;}</style>', unsafe_allow_html=True)
 pwd = st.text_input("أدخل الباسورد", type="password")
-  if pwd == "4455":
+if pwd == "4455":
   st.title("📚 مساعد الملازم الذكي")
   f = st.file_uploader("ارفع الملزمة (PDF)", type="pdf")
    import PyPDF2
