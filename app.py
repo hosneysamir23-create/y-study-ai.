4bab14996fa32452f02ae585882de1188898ead1
hosneysamir23-create <hosneysamir23@gmail.com>
2026-03-02 06:05:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ if pwd == "4455":
   st.title("📚 مساعد الملازم الذكي")
 f = st.file_uploader("ارفع الملزمة (PDF)", type="pdf")
 if f:
-pdf = PyPDF2.PdfReader(f)
+  pdf = PyPDF2.PdfReader(f)
 text = "".join([p.extract_text() for p in pdf.pages])
 st.success("✅ تم التحميل!")
 q = st.chat_input("اسألني أي سؤال...")
 if q:
-genai.configure(api_key="AIzaSyDETNhoieNKbhhq_zF_W0AVaGlCBrMct0g")
+  genai.configure(api_key="AIzaSyDETNhoieNKbhhq_zF_W0AVaGlCBrMct0g")
 model = genai.GenerativeModel('gemini-1.5-flash')
 res = model.generate_content(f"أجب بالعربية: {text[:10000]}\nالسؤال: {q}")
 st.write(res.text)
