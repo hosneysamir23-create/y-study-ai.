aa9494657c591febdaa22a01185858bd75086249
hosneysamir23-create <hosneysamir23@gmail.com>
2026-03-02 04:42:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ if pwd == "4455":
   st.title("📚 مساعد الملازم الذكي")
   f = st.file_uploader("ارفع الملزمة (PDF)", type="pdf")
   import PyPDF2
-   import google.generativeai as genai
+ import google.generativeai as genai
     pdf = PyPDF2.PdfReader(f)
       text = "".join([p.extract_text() for p in pdf.pages])
        st.success("تم تحميل الملف")
