e0f1f2dfa0255845e52f22a6ddc297d0becc3ba6
hosneysamir23-create <hosneysamir23@gmail.com>
2026-03-02 05:00:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ pwd = st.text_input("أدخل الباسورد", type="password")
 if pwd == "4455":
  st.title("📚 مساعد الملازم الذكي")
  f = st.file_uploader("ارفع الملزمة (PDF)", type="pdf")
-   if f:
+  if f:
     import PyPDF2
      import google.generativeai as genai
       pdf = PyPDF2.PdfReader(f)
