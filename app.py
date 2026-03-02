a4119bc09a49021051576a805a0c0d73d750d078
hosneysamir23-create <hosneysamir23@gmail.com>
2026-03-02 06:18:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if pwd == "4455":
 f = st.file_uploader("ارفع الملزمة (PDF)", type="pdf")
 if f is not None:
   try:
-reader = PyPDF2.PdfReader(f)
+  reader = PyPDF2.PdfReader(f)
 text = ""
 for page in reader.pages:
 text += page.extract_text()
